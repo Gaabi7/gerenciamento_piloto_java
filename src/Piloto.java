94d54f39c739c4c25004c02c98eff2e6e28b32fe
Euriksei <matheuseuriques777@gmail.com>
2025-05-29 22:15:04 -0300

    correcao

--- src/Piloto.java
+++ src/Piloto.java
@@ -1,7 +1,13 @@
 public class Piloto {
-    
+    private String nome;
+    private String equipe;
+    private int numEquipe;
 
-        
-        
+    public void escreverTXT(){
+        //escreve no arquivo
     }
+
+
+
 }
+
